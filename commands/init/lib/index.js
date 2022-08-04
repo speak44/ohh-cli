0292d3db57d620d8779215d14b6ff5c68c296ce9
speak44 <1246806407@qq.com>
2022-08-04 11:35:23 +0800

    0.0.3

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,7 +1,19 @@
 'use strict';
-
-function init(projectNmame, cmdObj) {
-    console.log(projectNmame,'cmdObj');
-    console.log('init', projectNmame, cmdObj.force, process.env.CLI_TARGET_PATH);
+const Command = require('@ohh-cli/command')
+const log = require('@ohh-cli/log');
+class InitCommand extends Command{
+    init(){
+        this.projectName = this._argv[0]|| '';
+        this.force =!!this._cmd._optionValues.force;
+        log.verbose('projectName', this.projectName);
+        log.verbose('force', this.force);
+    }
+    exec(){
+        console.log('init的业务逻辑');
+    }
 }
-module.exports = init;
+function init (argv){
+    return new InitCommand(argv)
+}
+module.exports = init 
+module.exports.InitCommand = InitCommand;
